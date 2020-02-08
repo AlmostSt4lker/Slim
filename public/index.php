6607076b2e25737627af2cfdedd5177f97e3db20
AlmostSt4lker <adamx851@gmail.com>
2020-02-08 21:30:39 +0100

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,8 @@
 <?php
 declare(strict_types=1);
 
-
-
-
-
 use Slim\Factory\AppFactory;
 use DI\Container;
-
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -19,7 +14,6 @@ $settings($container); //Passing in $container to settings
 $logger = require __DIR__.'/../app/logger.php'; // $logger = returned function
 $logger($container); //Passing in $container to logger
 
-
 //Set container on app
 AppFactory::setContainer($container);
 
@@ -28,14 +22,10 @@ $app = AppFactory::create();
 $view = require __DIR__.'/../app/views.php';
 $view($app);
 
-
-
 $middleware = require __DIR__.'/../app/middleware.php'; // $Middleware = returned function
 $middleware($app); //Passing in $app to Middleware function
 
 $routes = require __DIR__.'/../app/routes.php'; // $routes = returned function
 $routes($app); // Pass in $app to routes
-
-
 
 $app->run();
